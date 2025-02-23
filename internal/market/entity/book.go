96d4a311cf6eb127746bd2c3374f21f6628d1bbb
analopesdev <analopesdemorais@outlook.com>
2025-02-23 17:22:17 -0300

    add Book entity with order and transaction management; implement constructor for initialization

--- internal/market/entity/book.go
+++ internal/market/entity/book.go
@@ -1,1 +1,21 @@
 package entity
+
+import "sync"
+
+type Book struct {
+	Order           []*Order
+	Transaction     []*Transaction
+	IncomingOrders  chan *Order
+	ProcessedOrders chan *Order
+	Wg              *sync.WaitGroup
+}
+
+func NewBook(incomingOrders chan *Order, processedOrders chan *Order, wg *sync.WaitGroup) *Book {
+	return &Book{
+		Order:           []*Order{},
+		Transaction:     []*Transaction{},
+		IncomingOrders:  incomingOrders,
+		ProcessedOrders: processedOrders,
+		Wg:              wg,
+	}
+}
